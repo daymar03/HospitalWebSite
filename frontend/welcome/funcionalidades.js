10293e23b107628c433afc8ff89c6f8e084e92ed
Daymar Guerrero Santiago <daymarguerreros03@proton.me>
2024-11-06 07:45:00 +0000

    Update funcionalidades.js

--- frontend/welcome/funcionalidades.js
+++ frontend/welcome/funcionalidades.js
@@ -12,33 +12,4 @@ document.addEventListener('click', function(event) {
     }
 });
 
-function login() {
-            var username = document.getElementById("username").value;
-            var password = document.getElementById("myPassword").value;
 
-            if (username === "admin" && password === "admin") {
-                window.open('./admin_panel/admin.html', '_blank');
-            } else if (username === "secretaria" && password === "secretaria") {
-                window.open('./secretary_panel/secretaria.html', '_blank');
-            } else {
-                alert("Credenciales incorrectas");
-            }
-        }
-
-/*
-
-// Cambio de fondo 
-let images = [
-    'fondo.jpg',
-    'fondo1.png'
-];
-let currentIndex = 0;
-
-function changeBackground() {
-    document.body.style.backgroundImage = `url(${images[currentIndex]})`;
-    currentIndex = (currentIndex + 1) % images.length;
-}
-
-setInterval(changeBackground, 80000); // cambiar cada 5 segundos
-changeBackground(); // Inicial para cargar la primera imagen
-*/
