265c48d699b5d99d3f230e0898ea72273e1c1759
Daymar Guerrero Santiago <daymarguerreros03@proton.me>
2024-12-02 00:56:20 -0500

    Delete frontend/doctor directory

--- frontend/doctor/notificaciones/script.js
+++
@@ -1,7 +0,0 @@
-let cerrar = $(".close")
-
-cerrar.on("click", ()=>{
-  window.open("../index.html", "_self")
-})
-
-
--- frontend/doctor/script.js
+++
@@ -1,5 +0,0 @@
-let cerrar = document.querySelector(".close")
-
-cerrar.addEventListener("click", ()=>{
-  window.open("../login/index.html", "_self")
-})
